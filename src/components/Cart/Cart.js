c3ac79627826d7586b8d4e1e07631ccdf4dce468
vasanth1066 <vasanthanuvavi@gmail.com>
2024-04-23 15:16:19 +0530

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,21 @@
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const flag = false;
+  return (
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        {flag && (
+          <CartItem
+            item={{ title: "Test Item", quantity: 3, total: 18, price: 6 }}
+          />
+        )}
+      </ul>
+    </Card>
+  );
+};
+
+export default Cart;
